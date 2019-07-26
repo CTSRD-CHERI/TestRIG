b688feb592eaadb10ebfbca1d8a7c86da9c96e22
Peter Rugg <pdr32@cam.ac.uk>
2019-07-27 00:29:28 +0100

    Fix bug where TestRIG wouldn't detect if implementation trapped but not model

--- vengines/QuickCheckVEngine/src/RVFI_DII.hs
+++ vengines/QuickCheckVEngine/src/RVFI_DII.hs
@@ -145,6 +145,7 @@ instance Eq RVFI_DII_Execution where
     | rvfi_halt x /= 0 = (rvfi_halt x) == (rvfi_halt y)
     | rvfi_trap x /= 0 = ((rvfi_trap x) == (rvfi_trap y)) && (rvfi_pc_wdata x) == (rvfi_pc_wdata y)
     | otherwise = (maskUpper $ rvfi_exe_insn x) == (maskUpper $ rvfi_exe_insn y) &&
+                  (rvfi_trap x) == (rvfi_trap y) && (rvfi_halt x) == (rvfi_halt y) &&
                   (rvfi_rd_wdata x) == (rvfi_rd_wdata y) &&
                   (rvfi_mem_wmask x) == (rvfi_mem_wmask y) &&
                   ((rvfi_mem_wmask x == 0) || ((rvfi_mem_addr x) == (rvfi_mem_addr y))) &&
