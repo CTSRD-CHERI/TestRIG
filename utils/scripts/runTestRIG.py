f172ce8419fa1fe631f192c10c0f7ca68b56ad46
Franz Fuchs <franz.fuchs@cl.cam.ac.uk>
2021-08-05 09:42:30 +0100

    added --continue-on-fail to runTestRIG.py as well as to the run spec contracts jenkinsfile

--- utils/scripts/runTestRIG.py
+++ utils/scripts/runTestRIG.py
@@ -200,6 +200,8 @@ parser.add_argument('--no-shrink', action='count', default=0,
   help="Disable VEngine test case shrinking")
 parser.add_argument('--no-save', action='count', default=0,
   help="Don't ask to save files")
+parser.add_argument('--continue-on-fail', action='count', default=0,
+  help="Continue when encountering a failure")
 parser.add_argument('--test-len', metavar='LEN', default=None, type=auto_int,
   help="Tell vengine to generate tests up to LEN instructions long")
 
@@ -516,6 +518,8 @@ def spawn_vengine(name, mport, iport, arch, log):
       cmd += ['-S']
     if args.no_save:
       cmd += ['--no-save']
+    if args.continue_on_fail:
+      cmd += ['--continue-on-fail']
     if args.test_len:
       cmd += ['-L', str(args.test_len)]
     if args.test_include_regex:
